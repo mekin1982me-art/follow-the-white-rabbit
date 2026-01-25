82db42d23d6d60c7bbe2592e1ccd0cea012493c4
mekin1982me-art <mekin1982.me@gmail.com>
2026-01-25 11:58:24 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
             src="/logo-tr-temp.png"
             alt="Follow the White Rabbit Logo"
             style={{
-              width: "clamp(260px, 50vw, 360px)", // LOGO BOYUTU (küçük değil)
+              width: "clamp(320px, 50vw, 180px)", // LOGO BOYUTU (küçük değil)
               height: "auto",
               display: "block",
               margin: "0 auto",
